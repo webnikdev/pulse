ca3c84b2b4cbf2bd36e00bcecf1ef1e7f93c63ad
Mykola <webnikdevelopment@gmail.com>
2024-07-03 11:55:59 +0300

    Creating a slider using slick

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        adaptiveHeight: true,
-        autoplay: true,
-        autoplaySpeed: 2000
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>', 
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
     });
 });
